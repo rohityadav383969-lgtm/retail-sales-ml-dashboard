72f3cda37047205c595c2fd98961a241af5f6374
Rohit <rohityadav383969@gmail.com>
2026-03-05 15:23:04 +0530

    Fixed dataset path for Streamlit deployment

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,22 +1,10 @@
-'''import pandas as pd
-import os
-
-def load_data():
-    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    data_path = os.path.join(base_dir, "data", "retail_sales_dataset.csv")
-
-    df = pd.read_csv(data_path)
-    df.columns = df.columns.str.lower().str.strip()
-
-    return df'''
-
 import pandas as pd
 from src.logger import logger
 
 def load_data(path):
 
     try:
-        df = pd.read_csv("/Users/rohityadav/Desktop/sales_ml_project copy/data/retail_sales_dataset.csv")
+        df = pd.read_csv(path)
         logger.info("Dataset loaded successfully")
         return df
 
